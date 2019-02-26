6137f184492cce7164afe6b51688f87b2d1a7859
unknown <pvicensleon@gmail.com>
2019-02-26 11:19:34 -0300

    add more css

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
 	//insert todos from db to page
 	function addAllTodos(todos){
-		todos.forEach(function(todo){
+		todos.forEach((todo) => {
 			//duplicated code made into function
 			addSingleTodo(todo);
 			//_.sortBy(todo, ['name', 1]);
@@ -20,7 +20,7 @@ $(document).ready(function(){
 	//create new todo
 	//listens to keypress
 	$("#todoInput").keypress(function(event){	
-		var userInput = $("#todoInput").val();	
+		let userInput = $("#todoInput").val();	
 		//listen for enter key and check that input is not empty
 		if(event.which === 13 && userInput.length != 0){
 			//send request to create todo
@@ -38,15 +38,15 @@ $(document).ready(function(){
 
 	//check off specific todos when clicked
 	//use on not click function to listen on already created item
-	$(".list").on("click", "li", function(){	
+	$("ul").on("click", "li", function(){	
 		//get id from db
-		var clickedId = $(this).data("id");
+		let clickedId = $(this).data("id");
 		//get completed status from db
-		var isDone = $(this).data("completed");
+		let isDone = $(this).data("completed");
 		//cant call this inside then function, out of scope
-		var update = $(this);
+		let update = $(this);
 		//put request !isDone so true => false
-		var updateData = {completed: !isDone}
+		let updateData = {completed: !isDone}
 		$.ajax({
 			method: "PUT",
 			url: "/api/todos/" + clickedId,
@@ -63,12 +63,12 @@ $(document).ready(function(){
 
 	//remove todo when click on X
 	//add listener on list class, only on span elements
-	$(".list").on("click", "span", function(event){	
+	$("ul").on("click", "span", function(event){	
 		//stops the bubling process
 		event.stopPropagation();
 		//add id
-		var clickedId = $(this).parent().data("id");
-		var parent = $(this).parent();
+		let clickedId = $(this).parent().data("id");
+		let parent = $(this).parent();
 		$.ajax({
 			method: "DELETE",
 			url: "/api/todos/" + clickedId 
@@ -87,7 +87,7 @@ $(document).ready(function(){
 
 function addSingleTodo(todoData){
 	//create our new todo
-	var newTodo = $("<li class='task' >" + "<span>X </span>" + todoData.name + "</li>");
+	let newTodo = $("<li><span><i class='far fa-trash-alt'></i></span>" + todoData.name + "</li>");
 	//store todo id
 	newTodo.data("id", todoData._id);
 	//add completed boolean
@@ -97,5 +97,5 @@ function addSingleTodo(todoData){
 		newTodo.addClass("completed");
 	}
 	//append new todo
-	$(".list").append(newTodo);
+	$("ul").append(newTodo);
 };
