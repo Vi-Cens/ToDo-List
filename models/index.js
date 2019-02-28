6dbec892901106c8e8bf65ace6478dbd6ff890aa
unknown <pvicensleon@gmail.com>
2019-02-27 21:19:46 -0300

    add mongolab

--- models/index.js
+++ models/index.js
@@ -1,6 +1,11 @@
 var mongoose = require("mongoose")
 mongoose.set("debug", true);
-mongoose.connect("mongodb://localhost/todo-api-db");
+
+//local mongodb
+//mongoose.connect("mongodb://localhost/todo-api-db");
+
+//heroku mongolab db
+mongoose.connect('mongodb://pvicens:p123456@dbh43.mlab.com:27437/todo-list');
 
 mongoose.Promise = Promise; //so it can use catch and then
 
